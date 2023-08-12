58d577cfabe702f85f7d06d079f0e75ae42c5e57
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-08-12 14:58:02 +0545

    fibonacci

---
+++ DAA/fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fibonacciSeries(int a){
+    int firstFibonacciTerm, secondFibonacciTerm, count=0, newFibonacciTerm;
+    firstFibonacciTerm = 0;
+    secondFibonacciTerm = 1;
+    printf("The sequence upto %d  term is \n", a);
+    printf("%d \t %d \t", 0, 1);
+    while(a > 2){
+        newFibonacciTerm = firstFibonacciTerm + secondFibonacciTerm;
+        printf("%d \t", newFibonacciTerm);
+        firstFibonacciTerm = secondFibonacciTerm;
+        secondFibonacciTerm = newFibonacciTerm;
+        count++;
+        a--;
+    }
+    printf("\n Loop runned %d times", count);
+    return 0;
+}
+int main(){
+    fibonacciSeries(20);
+    return 0;
+}
